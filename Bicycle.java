9bcb88095e69f5c4eec3a9e291bccab774e923b7
Maura Thompson <thomaura@gmail.com>
2022-02-08 15:05:41 -0500

    Add Bicycle and BicycleDemo source code and compiled files

---
+++ Bicycle.java
@@ -0,0 +1,54 @@
+// Cannot run this class in CLI because "main" not defined
+class Bicycle {
+    
+    int cadence = 0;
+    int speed = 0;
+    int gear = 1;
+
+    void changeCadence(int newValue) {
+        cadence = newValue;
+    }
+
+    void changeGear(int newValue) {
+        gear = newValue;
+    }
+
+    void speedUp(int increment) {
+        speed = speed + increment;
+    }
+
+    void applyBrakes(int decrement) {
+        speed = speed - decrement;
+    }
+
+    void printStates() {
+        System.out.println("cadence: " +
+        cadence + ", speed: " + speed +
+        ", gear: " + gear);
+    }
+
+}
+
+// run with `java BicycleDemo` in CLI
+class BicycleDemo {
+    public static void main(String[] args) {
+        
+        Bicycle bike1 = new Bicycle();
+        Bicycle bike2 = new Bicycle();
+
+        bike1.changeCadence(50);
+        bike1.printStates();
+        bike1.speedUp(2);
+        bike1.printStates();
+
+        bike2.changeCadence(50);
+        bike2.printStates();
+        bike2.speedUp(10);
+        bike2.printStates();
+        bike2.changeGear(3);
+        bike2.printStates();
+        bike2.applyBrakes(15);
+        bike2.printStates();
+
+    }
+}
