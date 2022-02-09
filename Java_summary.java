2cfa63cb849d8e617192d00caa9bbb1795e2cfd4
Maura Thompson <thomaura@gmail.com>
2022-02-08 20:25:02 -0500

    Update Java_summary.java

--- Java_summary.java
+++ Java_summary.java
@@ -1,4 +1,22 @@
-// #### NAMING CONVENTIONS ####
+// // #### GENERAL #### //
+// Whitespace doesn't have syntax meaning
+// Semicolons
+// => Use after expressions, method calls, variable declarations
+// => Don't need semicolon after "blocks"
+//      e.g, if stmts, loops, class/method definitions
+// => Code won't compile if semicolons are missing
+
+
+// //  #### TYPES OF VARIABLES #### //
+// https://docs.oracle.com/javase/tutorial/java/nutsandbolts/variables.html
+
+// => Instance Variables (Non-Static Fields)
+// => Class Variables (Static Fields)
+// => Local Variables
+// => Parameters
+
+
+// // #### NAMING CONVENTIONS #### //
 // https://docs.oracle.com/javase/tutorial/java/nutsandbolts/variables.htmls
 
 // variable names are case-sensitive
@@ -13,3 +31,6 @@
 // if var is more than one word, use camelCase
 // if var stores constant value, use all caps, words separated by underscore
 // never use underscore for anything else!
+
+
+// // #### DATA TYPES #### //
