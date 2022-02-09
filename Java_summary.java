37346001c0b80bda80062024d05161e452788f03
Maura Thompson <thomaura@gmail.com>
2022-02-08 19:11:14 -0500

    Add Java summary notes

---
+++ Java_summary.java
@@ -0,0 +1,15 @@
+// #### NAMING CONVENTIONS ####
+// https://docs.oracle.com/javase/tutorial/java/nutsandbolts/variables.htmls
+
+// variable names are case-sensitive
+// => white space not permitted in variable names
+// => "$" and "_" technically allowed in var names, but
+// => strong convention is to NOT use "$" or "_" in var names
+// => can be unlimited length
+
+// use full words for variables instead of cryptic abbreviations
+// variable name must not be keyword or reserved word
+// if var name is one word, use all lowercase
+// if var is more than one word, use camelCase
+// if var stores constant value, use all caps, words separated by underscore
+// never use underscore for anything else!
